fd81fe1a3380a28bd45f81473e409441bb098118
Mounish Vatti <mounishvatti2002@gmail.com>
2025-01-29 22:20:34 +0530

    change

--- apps/excalidraw-frontend/app/canvas/[roomId]/page.tsx
+++ apps/excalidraw-frontend/app/canvas/[roomId]/page.tsx
@@ -1,13 +1,6 @@
-
 import { RoomCanvas } from "@/components/RoomCanvas";
 
-export default async function CanvasPage({ params }: {
-    params: {
-        roomId: string
-    }
-}) {
-    const roomId = (await params).roomId;
 
-    return <RoomCanvas roomId={roomId} />
-   
+export default function CanvasPage({ params }: any) {
+    return <RoomCanvas roomId={params.roomId as string} />;
 }
