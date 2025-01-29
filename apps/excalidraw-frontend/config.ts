63e318d7a02480ac9e7c058d25144afe4fa825be
Mounish Vatti <mounishvatti2002@gmail.com>
2025-01-29 20:34:24 +0530

    changed urls

--- apps/excalidraw-frontend/config.ts
+++ apps/excalidraw-frontend/config.ts
@@ -1,2 +1,2 @@
-export const HTTP_BACKEND = "http://localhost:4001"
-export const WS_URL = "ws://localhost:8080";
+export const HTTP_BACKEND = "http://httpbe.doodlelabs.space"
+export const WS_URL = "http://wsbe.doodlelabs.space";
--- apps/web/app/config.ts
+++ apps/web/app/config.ts
@@ -1,3 +1,3 @@
 
-export const BACKEND_URL = "http://localhost:4001";
-export const WS_URL = "ws://localhost:8080";
+export const BACKEND_URL = "http://httpbe.doodlelabs.space"
+export const WS_URL = "http://wsbe.doodlelabs.space";
