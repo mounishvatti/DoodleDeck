c3093aefb05a2d1d76d628d7d8f7aac98d3c1f5e
Mounish Vatti <mounishvatti2002@gmail.com>
2025-01-30 19:44:41 +0530

    upgraded to https

--- apps/excalidraw-frontend/config.ts
+++ apps/excalidraw-frontend/config.ts
@@ -1,2 +1,2 @@
-export const HTTP_BACKEND = "http://httpbe.doodlelabs.space"
-export const WS_URL = "http://wsbe.doodlelabs.space/websocket";
+export const HTTP_BACKEND = "https://httpbe.doodlelabs.space"
+export const WS_URL = "https://wsbe.doodlelabs.space/websocket";
