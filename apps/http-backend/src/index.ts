b24d8cead55e97d2b5ab6bac52ddcb390e90f7ba
Mounish Vatti <mounishvatti2002@gmail.com>
2025-02-02 16:13:22 +0530

    testing pm2 and cicd

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -30,7 +30,7 @@ app.use(cors({
 
 app.get("/", (req, res) => {
     res.json({
-        message: "Hello from doodle-deck's http-backend"
+        message: "Hello from doodle-deck's http-backend!"
     })
 });
 
